e36ff94e9e297e788266609e0a43c14967b73116
David Nogueira <david.nogueira.51@gmail.com>
2018-11-30 16:06:13 +0100

    moteur recherche

--- products.php
+++ products.php
@@ -1,5 +1,54 @@
 <?php require_once('inc/bdd.php'); ?>
-<?php      
+
+
+<form>
+    <label for="recherche">Recherche :</label>
+    <input type="text" name="recherche">
+    <button>Envoyer</button>
+</form>
+
+<?php
+if (!empty($_GET['recherche'])) {
+    echo "recherche ok";
+
+    $select = $connexion->prepare('SELECT * FROM products WHERE product_name LIKE :recherche');
+    $select->bindValue(':recherche', '%' .$_GET['recherche'] . '%');
+    $select->execute();
+
+    $articles = $select->fetchAll();
+    foreach($articles as $article){
+    ?>
+        <article>       
+            <h2>
+                <?php echo $article['product_name']; ?>
+            </h2>
+            créé le <?php echo  $article['creation_date']; ?>
+            <p><?php echo  $article['price']; ?></p>
+            <p><img src="images_uploadees/<?php echo $article['picture'];?>"></p>
+        </article>
+
+    <?php
+    
+    } 
+} else {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     $resultat = $connexion->query('SELECT * FROM products ORDER BY creation_date DESC LIMIT 10 OFFSET 0');//LIMIT 0,10
     //LIMIT 5,10 est équivalent à LIMIT 10 OFFSET 5)
@@ -13,7 +62,10 @@
             </h2>
             créé le <?php echo  $product['creation_date']; ?>
             <p><?php echo  $product['price']; ?></p>
+            <p><img src="images_uploadees/<?php echo $product['picture'];?>"></p>
         </article>
+
     <?php
     }
+}
     ?>
