f07181beb3c8fc016513e8dbb4b993ac5a1d640b
MPWebdesign <melanieplantey@gmail.com>
2018-11-30 14:13:56 +0100

    suppression image

--- image.php
+++ image.php
@@ -1,5 +1,115 @@
 <?php
 require_once('inc/bdd.php');
+if(!empty($_GET['delete']) && is_numeric($_GET['delete'])){
+	$image = $_GET['image'];
+	$delete = $connexion->prepare('DELETE FROM slider WHERE id = :id'); 
+	$delete->bindValue(':id', $_GET['delete']);
+	if($delete->execute()){
+	 if(file_exists('images_uploadees/miniatures/' . $image)){
+		unlink('images_uploadees/miniatures/' . $image);
+		}
+	 if(file_exists('images_uploadees/' . $image)){
+		unlink('images_uploadees/' . $image);
+		}
+	}
+}
+if(!empty($_FILES)){
+	// on m'a envoyé au moins un fichier, je peux faire le traitement 
+
+	// je vérifie que le transfert s'est bien passé :
+	if($_FILES['fichier']['error'] == 0){
+		// so error = 0, c'est bon 
+
+		// je vérifie la taille du fichier
+		// 1 ko = 1024 octets
+		// 1 Mo = 1 048 576 octets
+
+		// je veux limiter mes fichiers à 500 ko
+		$maxSize = 500 * 1024;  // 500 ko en octets
+		if($_FILES['fichier']['size'] <= $maxSize){
+			// le fichier a une taille acceptable
+
+			// je vérifie l'extension du fichier 
+			$fileInfo = pathinfo($_FILES['fichier']['name']); 
+			/*var_dump($fileInfo); */
+			$extension = strtolower($fileInfo['extension']); 
+
+			$extensionsAutorisees = ['jpg', 'jpeg', 'png']; 
+
+			if(in_array($extension, $extensionsAutorisees)){
+
+				// l'extension est ok, on peut proceder au transfert définitif du fichier 
+				$newName = md5(uniqid(rand(), true)); 
+				/*echo $newName; */
+
+				// ------------------ Création d'une miniature
+				// je décide que mes miniatures seront de 100px de large
+				$newWidth = 100; 
+
+				if($extension == 'jpg' || $extension === 'jpeg'){
+					$newImage = imagecreatefromjpeg($_FILES['fichier']['tmp_name']); 
+				}
+				elseif($extension === 'png'){
+					$newImage = imagecreatefrompng($_FILES['fichier']['tmp_name']); 
+				}
+				
+				// je vais devoir calculer la hauteur de ma miniature
+				// largeur originale (en px) 
+
+				$oldWidth = imagesx($newImage); 
+
+				// hauteur originale 
+
+				$oldHeight = imagesy($newImage); 
+
+				// calcul de la nouvelle hauteur 
+
+				$newHeight = ($oldHeight * $newWidth) / $oldWidth; 
+
+				// je crée une nouvelle image avec les dimensions (nouvelle largeur et nouvelle hauteur)
+
+				$miniature = imagecreatetruecolor($newWidth, $newHeight); 
+
+				// on remplit la miniature à partir de l'image originale 
+
+				imagecopyresampled($miniature, $newImage, 0, 0, 0, 0, $newWidth, $newHeight, $oldWidth, $oldHeight); 
+
+				// chemin vers le dossier ou je stocke mes miniatures
+
+				$folder = 'images_uploadees/miniatures/'; 
+
+				if($extension === 'jpg' || $extension === 'jpeg'){
+					imagejpeg($miniature, $folder . $newName . '.' .$extension); 
+				}
+				elseif($extension === 'png'){
+					imagepng($miniature, $folder . $newName . '.' .$extension); 
+				}
+				elseif($extension === 'gif'){
+					imagegif($miniature, $folder . $newName . '.' .$extension); 
+				}
+
+				// je peux transferer le fichier sur le serveur 
+				move_uploaded_file($_FILES['fichier']['tmp_name'],
+			 'images_uploadees/' .$newName. '.' .$extension); 
+			}
+
+			// se connecter à la base et effectuer la requête pour insérer les infos dans la base
+
+			$insert = $connexion->query("INSERT INTO slider (image) VALUES ('$newName.$extension')");
+			if($insert){
+				echo 'image uploadée'; 
+			}
+			else{
+				echo 'upload ko'; 
+			}
+		}
+		else{
+			echo 'mauvaise extension'; 
+			
+	}
+		}
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +124,7 @@ require_once('inc/bdd.php');
 	<div class="container">
         <div class="row">
             <div class="col-md-8">
-				<form action="traitement.php" method="POST" enctype="multipart/form-data">
+				<form  method="POST" enctype="multipart/form-data">
 					<br><label><strong>Ajouter une image : </strong></label>
 					<input type="file" name="fichier" class="form-control">
 					<br><button type="submit" class="btn btn-primary">Envoyez</button><br>
@@ -27,7 +137,7 @@ require_once('inc/bdd.php');
                     $slider = $resultat->fetchAll();
                      foreach($slider as $image){
                          ?>
-                        <div value="<?= $image['id'] ?>" ><img src="images_uploadees/miniatures/<?= $image['image'] ?>"> <a href="#">Supprimer l'image</a></div>
+                        <div value="<?= $image['id'] ?>" ><img src="images_uploadees/miniatures/<?= $image['image'] ?>"> <a href="image.php?delete=<?= $image['id'] ?>&image=$image['image']">Supprimer l'image</a></div>
                         <?php
                         }
                     ?>
