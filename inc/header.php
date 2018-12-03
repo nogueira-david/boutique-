5e06e03d4e881a5f6a52101b286033ff30a0f1a4
Jean <jeanmickhail@gmail.com>
2018-12-03 09:43:12 +0100

    monk

--- inc/header.php
+++ inc/header.php
@@ -1,45 +1,64 @@
-
-    <ul>
-      <?php if(!isset($_SESSION['id'])){ ?>
-        <li>
-            <a href="index.php">Home<span>(current)</span></a>
-        </li>
-        <li>
-            <a href="products.php">Products</a>
-        </li>
-        <li>
-            <a href="contact.php">Contact</a>
-        </li>
-        <li>
-            <a href="connexion.php">Log in</a>
-        </li>
-        <li>
-            <a href="inscription.php">Registration</a>
-        </li>
-      <?php } ?>
-      <?php 
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title></title>
+  </head>
+  <body>
+    
+  <div class="container">
+  <div class="col-8 m-auto">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <a class="navbar-brand" href="#">###</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+  <?php if(!isset($_SESSION['id'])){ ?>
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+            <a class="nav-link" href="products.php">Products</a>
+      </li>
+      <li class="nav-item active">
+            <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+      <li class="nav-item active">
+            <a class="nav-link" href="connexion.php">Log in</a>
+      </li>
+      <li class="nav-item active">
+            <a class="nav-link" href="inscription.php">Registration</a>
+      </li>
+  <?php }
       if(isset($_SESSION['id'])){
           ?>
-          <li>
-            <a href="profil.php">Profile</a>
+          <li class="nav-item active">
+            <a class="nav-link" href="profil.php">Profile</a>
           </li>
-          <li>
-            <a href="demande_reset.php">Change your password</a>
+          <li class="nav-item active">
+            <a class="nav-link" href="demande_reset.php">Change your password</a>
           </li>
-          <li>
-            <a href="deconnexion.php">Sign out</a>
+          <li class="nav-item active">
+            <a class="nav-link" href="deconnexion.php">Sign out</a>
           </li>
           <?php if($_SESSION['role'] === 'ROLE_ADMIN'){ ?>
-            <li>
-              <a href="admin_management.php">Management</a>
+            <li class="nav-item active">
+              <a class="nav-link" href="admin_management.php">Management</a>
             </li>
             <?php } ?>
           <?php if($_SESSION['role'] === 'ROLE_ADMIN' || $_SESSION['role'] === 'ROLE_AUTEUR'){ ?>
-            <li>
-              <a href="ajout_article.php">Adding article</a>
+            <li class="nav-item active">
+              <a class="nav-link" href="ajout_article.php">Adding article</a>
             </li>
             <?php } ?>
           <?php
       }
       ?>
-    </ul
+    </ul>
+  </div>
+</nav>
+</div>
+</div>
--- index.php
+++ index.php
@@ -31,5 +31,7 @@ include('inc/header.php');
 		<button>Connection</button>
 	</form>
 	<a href="deconnexion.php?deco=set"><button>Deconnection</button></a>
+
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
